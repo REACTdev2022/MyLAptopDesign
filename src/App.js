caa761e80798c437f68a0438bd5b3cf453c33bb4
AFRIDE IQUBAL <48866141+REACTdev2022@users.noreply.github.com>
2022-04-10 16:51:19 +0530

    finish

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Background from "./Components/Background";
+import Laptop from "./Components/Laptop";
+
+const App = () => {
+  return (
+    <>
+      <Background />
+      <Laptop />
+    </>
+  );
+};
+
+export default App;
